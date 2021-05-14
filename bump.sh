8f88b69c76b9d29b6cb82f726fa4bb09f8e0e3a5
Dave Xiong <hxiong@pccwglobal.com>
2021-05-15 00:25:14 +1000

    add git commit

--- bump.sh
+++ bump.sh
@@ -14,4 +14,24 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 
 CURRENT_VERSION=`mvn -s $DIR/settings.xml org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout`
-echo "version:"+$CURRENT_VERSION
+echo "version:"$CURRENT_VERSION
+
+IFS='. ' read -r -a versions <<< "$CURRENT_VERSION"
+VERSION_INC=$((versions[1]%2==1?2:1))
+NEW_VERSION=${versions[0]}.$[versions[1]+VERSION_INC].0
+
+echo -e $CURRENT_VERSION"==>"$NEW_VERSION
+
+mvn -s $DIR/settings.xml versions:set -DnewVersion=$NEW_VERSION -DprocessAllModules
+if [ $? -eq 0 ]
+then
+  echo -e "mvn version $NEW_VERSION successfully"
+else
+  echo -e "mvn version $NEW_VERSION failed"
+  exit 1
+fi
+git add .
+git commit -m "RELEASE:$NEW_VERSION"
+git tag -a $NEW_VERSION -m $NEW_VERSION
+git push
+git push --tags
