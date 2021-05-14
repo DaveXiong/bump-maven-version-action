7043c6a65e2037e79e0be31cb94a3e424499c679
Dave Xiong <hxiong@pccwglobal.com>
2021-05-14 23:42:09 +1000

    test

--- bump.sh
+++ bump.sh
@@ -4,3 +4,6 @@ echo "hello"
 
 git status
 git log
+
+CURRENT_VERSION=`./mvnw org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout`
+echo "version:"+$CURRENT_VERSION
